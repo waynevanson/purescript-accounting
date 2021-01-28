ce2147b70e11a93580d39e93ae0d78b770086800
Wayne Van Son <waynevanson@gmail.com>
2021-01-28 18:53:52 +0000

    feat: creates basic lib

---
+++ src/Accounting.purs
@@ -0,0 +1,63 @@
+module Accounting (Value, Transaction, credit, debit, transaction, Entry) where
+
+import Prelude
+import Data.Accounting.Internal (NonZero, nonZero)
+import Data.Bifunctor (bimap)
+import Data.Date (Date)
+import Data.Either (Either(..))
+import Data.Filterable (partitionMap)
+import Data.List (List)
+import Data.List.NonEmpty (NonEmptyList, fromList)
+import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple(..))
+
+-- Data
+data Value
+  = Credit NonZero
+  | Debit NonZero
+
+-- The sum of all credits and debits Value are equal
+data Transaction
+  = Transaction
+    { credits :: NonEmptyList Value
+    , date :: Date
+    , debits :: NonEmptyList Value
+    }
+
+data Entry
+  = Entry
+    { account :: Account
+    , description :: String
+    , value :: Value
+    }
+
+data Account
+  = Asset
+  | Liability
+  | Expense
+  | Revenue
+  | Equity
+
+-- Constructors
+--
+-- | Create a `Credit` from an `Int`.
+credit :: Int -> Maybe Value
+credit = nonZero >>> map Credit
+
+-- | Create a `Debit` from an `Int`.
+debit :: Int -> Maybe Value
+debit = nonZero >>> map Debit
+
+-- | Create an abstraction
+transaction :: Date -> List Entry -> Maybe Transaction
+transaction date =
+  partitionMap
+    ( \(Entry { value }) -> case value of
+        Credit _ -> Left value
+        Debit _ -> Right value
+    )
+    >>> (\{ left, right } -> Tuple left right)
+    >>> bimap fromList fromList
+    >>> case _ of
+        Tuple (Just credits) (Just debits) -> Just $ Transaction { date, credits, debits }
+        otherwise -> Nothing
