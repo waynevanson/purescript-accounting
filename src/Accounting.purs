3f30edb84d704a5710dd27a3854307581d11ea2e
Wayne Van Son <waynevanson@gmail.com>
2021-01-29 17:12:50 +0000

    fix: use differnt types for debits and credits

--- src/Accounting.purs
+++ src/Accounting.purs
@@ -1,34 +1,44 @@
-module Accounting (Value, Transaction, credit, debit, transaction, Entry) where
+module Accounting (Value, Transaction, credit, debit, transaction, Entry, Credit, Debit) where
 
 import Prelude
-import Data.Accounting.Internal (NonZero, nonZero)
+import Data.Accounting.Internal (NonZero(..), nonZero)
 import Data.Bifunctor (bimap)
+import Data.Bitraversable (bifoldl, bitraverse)
 import Data.Date (Date)
-import Data.Either (Either(..))
+import Data.Either (Either)
 import Data.Filterable (partitionMap)
+import Data.Foldable (foldl)
 import Data.List (List)
 import Data.List.NonEmpty (NonEmptyList, fromList)
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe)
 import Data.Tuple (Tuple(..))
 
--- Data
-data Value
-  = Credit NonZero
-  | Debit NonZero
+newtype Credit
+  = CR NonZero
 
--- The sum of all credits and debits Value are equal
-data Transaction
-  = Transaction
-    { credits :: NonEmptyList Value
-    , date :: Date
-    , debits :: NonEmptyList Value
-    }
+derive instance eqCredit :: Eq Credit
+
+newtype Debit
+  = DR NonZero
+
+derive instance eqDebit :: Eq Debit
+
+type Value
+  = Either Credit Debit
 
 data Entry
   = Entry
     { account :: Account
     , description :: String
     , value :: Value
+    }
+
+-- The sum of all credits and debits Value are equal
+data Transaction
+  = Transaction
+    { credits :: NonEmptyList Credit
+    , date :: Date
+    , debits :: NonEmptyList Debit
     }
 
 data Account
@@ -38,27 +48,29 @@ data Account
   | Revenue
   | Equity
 
--- Constructors
---
 -- | Create a `Credit` from an `Int`.
-credit :: Int -> Maybe Value
-credit int = nonZero >>> map Credit $ int
+credit :: Int -> Maybe Credit
+credit int = nonZero >>> map CR $ int
 
 -- | Create a `Debit` from an `Int`.
-debit :: Int -> Maybe Value
-debit int = nonZero >>> map Debit $ int
+debit :: Int -> Maybe Debit
+debit int = nonZero >>> map DR $ int
 
--- | Create an abstraction
+-- | Create a `Transaction` using a `Date` and a list of `Entry`'s.
 transaction :: Date -> List Entry -> Maybe Transaction
-transaction date entries =
-  partitionMap
-    ( \(Entry { value }) -> case value of
-        Credit _ -> Left value
-        Debit _ -> Right value
-    )
-    >>> (\{ left, right } -> Tuple left right)
-    >>> bimap fromList fromList
-    >>> case _ of
-        Tuple (Just credits) (Just debits) -> Just $ Transaction { date, credits, debits }
-        otherwise -> Nothing
-    $ entries
+transaction date entries = do
+  tcrsdrs <-
+    partitionMap (\(Entry { value }) -> value)
+      >>> (\{ left, right } -> Tuple left right)
+      >>> bitraverse fromList fromList
+      $ entries
+  -- ensure the difference between both sides is not zero
+  _ <-
+    pure
+      $ bimap (map \(CR (NonZero i)) -> i) (map \(DR (NonZero i)) -> i)
+      >>> bimap (foldl (+) 0) (foldl (+) 0)
+      >>> bifoldl (-) (+) 0
+      $ tcrsdrs
+  -- deconstruct args
+  Tuple credits debits <- pure tcrsdrs
+  pure $ Transaction { date, credits, debits }
